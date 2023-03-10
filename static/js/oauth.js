ae7ad1ff420e10f50c0384a0103ca333a603ae8f
HThorne <hathorne@ucdavis.edu>
2023-03-09 23:45:46 -0800

    Fixed render bug

---
+++ static/js/oauth.js
@@ -0,0 +1,63 @@
+/* exported gapiLoaded */
+/* exported gisLoaded */
+/* exported handleAuthClick */
+/* exported handleSignoutClick */
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '348082355065-ghndkd0dserle2v34n4lbmkamsm3sjoi.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDoDns2vq9faPJbE10iVsLsmdLM8xRgvBI';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/tasks';
+
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+gapi.load('client', initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+});
+gapiInited = true;
+maybeEnableButtons();
+}
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+});
+gisInited = true;
+maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+if (gapiInited && gisInited) {
+    // document.getElementById('authorize_button').style.visibility = 'visible';
+}
+}
+
---
+++ static/js/tasklists.jsx
@@ -0,0 +1,101 @@
+function MainPage() {
+    const [taskList, setTaskList] = React.useState([]);
+
+    async function fetchTaskLists() {
+        let response;
+        try {
+            response = await gapi.client.tasks.tasklists.list({
+            'maxResults': 10,
+            });
+        } catch (err) {
+            document.getElementById('content').innerText = err.message;
+            return;
+        }
+        const taskLists = response.result.items;
+        
+        setTaskList(taskLists)
+    }
+    
+
+    function handleAuthClick() {
+        tokenClient.callback = async (resp) => {
+          if (resp.error !== undefined) {
+            throw (resp);
+          }
+          await fetchTaskLists();
+          };
+  
+  
+        if (gapi.client.getToken() === null) {
+          // Prompt the user to select a Google Account and ask for consent to share their data
+          // when establishing a new session.
+          tokenClient.requestAccessToken({prompt: 'consent'});
+        } else {
+          // Skip display of account chooser and consent dialog for an existing session.
+          tokenClient.requestAccessToken({prompt: ''});
+        }
+      }
+    
+    return (
+    <React.Fragment>
+      <button id="authorize_button" onClick={ handleAuthClick }>Authorize</button>
+      <TaskListView taskList={taskList}/>
+    </React.Fragment>
+    )
+}
+
+function TaskListView(props) {
+  const taskLists = []
+  
+  for (const list of props.taskList) {
+    taskLists.push(
+        <TasksView list={ list } key={list.id}/>
+    )
+  }
+  
+  return (
+    <div> { taskLists } </div>
+  )
+}
+
+
+
+function TasksView(props) {
+    const [tasks, setTasks] = React.useState([]);
+    
+    React.useEffect(() => {
+        async function fetchTasks() {
+            let response;
+            try {
+                response = await gapi.client.tasks.tasks.list({
+                    'tasklist': props.list.id
+                });
+            } catch (err) {
+                document.getElementById('content').innerText = err.message;
+                return;
+            }
+            const tasksResult = response.result.items;
+            setTasks(tasksResult)
+        }
+        fetchTasks()
+    }, [props.list]);
+
+
+
+    const tasksElements = []
+
+    for (const task of tasks) {
+        tasksElements.push(
+            <li key={task.id}>{ task.title }</li>
+        )
+    }
+
+    return (
+        <React.Fragment>
+            <h2> {props.list.title} </h2>
+            <ul> { tasksElements } </ul>
+        </React.Fragment>
+    )
+}
+
+ReactDOM.render(<MainPage />, document.querySelector('#all-lists'));
