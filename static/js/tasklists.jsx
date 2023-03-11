8c9e0f825eb295aaac4a2d9761882c644e463b6d
HThorne <hathorne@ucdavis.edu>
2023-03-11 03:58:54 -0800

    Added Input HTML for Add New Task

--- static/js/tasklists.jsx
+++ static/js/tasklists.jsx
@@ -111,6 +111,16 @@ function TasksView(props) {
                         <table className="table table-hover table-borderless align-middle"> 
                             <tbody>
                             { tasksElements } 
+                            <tr>
+                                <th scope="row"></th>
+                                    <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                                        </svg>
+                                    </td>
+                                    <td colSpan="2"><input type="text" className="form-control border-0" placeholder="Add a new task" aria-label="Add task">
+                                        </input>
+                                    </td>
+                            </tr>
                             </tbody>
                         </table>
                     </div>
